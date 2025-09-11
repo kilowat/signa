0c03d61fd44f5ec1edd383cbe777584a1147d794
Kilowat <kadantsev.sergey@gmail.com>
2025-09-11 09:28:28 +0300

    work

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import * as esbuild from 'esbuild'
-
+import { copyFileSync } from "fs";
 
 await esbuild.build({
     entryPoints: ["src/index.js"],
@@ -10,5 +10,6 @@ await esbuild.build({
     minify: true,
     sourcemap: false,
 })
+copyFileSync("src/signa.d.ts", "dist/signa.d.ts");
 
 console.log("✅ Build success");
--- examples/todo/src/main.js
+++ examples/todo/src/main.js
@@ -1,57 +1,80 @@
 const { defComponent, defStore, eventBus } = signa;
 
-defStore('todo', ({ signal }) => {
-  const items = signal([]);
+defStore('todo', (ctx) => {
+  const { signal } = ctx;
+  /** @type {Signal<Todo[]>} */
+  const items = signal([
+    {
+      id: 1,
+      name: 'test',
+      completed: false,
+    },
+    {
+      id: 2,
+      name: 'test2',
+      compoented: false,
+    }
+  ]);
 
-  const add = ({ id, name } = todo) => {
-    items.value = [...items.value, todo];
+  const toggle = (id) => {
+    items.value = items.value.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
   };
 
-  return { items, add };
+  const add = (todo) => {
+    items.value = [...items.value, todo]
+  };
+
+  return {
+    items,
+    add,
+    toggle,
+  }
 });
 
 defComponent('task-input', (ctx) => {
   const { html, signal, store } = ctx;
-
   const todoStore = store('todo');
   const inputValue = signal('');
 
   const addItem = () => {
-    const newTodo = {
-      id: Date.now(),
-      title: inputValue.value,
-      completed: false,
-    };
-    todoStore.add(newTodo);
-    inputValue.value = '';
-  };
+    todoStore.add({ id: Date.now().toString(), name: inputValue.value });
+  }
 
   const onInputChange = (event) => {
     inputValue.value = event.target.value;
+  }
+
+  return () => html`
+      <input
+        .value=${inputValue}
+        oninput="${onInputChange}" 
+        type="text" 
+        placeholder="Новая задача..."
+        class="flex-grow border rounded-l-md px-3 py-2 focus:outline-none focus:ring focus:border-blue-400">
+      <button
+        onclick='${addItem}'
+        class="bg-blue-500 text-white px-4 py-2 rounded-r-md hover:bg-blue-600">
+        Добавить
+      </button>`
+})
+
+defComponent('todo-list', (ctx) => {
+  const { html, store, htmlFor } = ctx;
+  const todoStore = store('todo');
+
+  const buildItem = item => {
+    const html = htmlFor(buildItem, item.id);
+    return html`
+    <div>
+      <span>id: ${item.id} status: ${item.completed}</span>
+      <button onclick="${() => todoStore.toggle(item.id)}">click</button>
+    </div>`;
   };
 
   return () => html`
-    <div style="display:flex; gap: 4px;">
-      <input
-        .value=${inputValue}
-        oninput=${onInputChange}
-        type="text"
-        placeholder="Новая задача..."
-        style="flex-grow:1; padding:4px;">
-      <button onclick=${addItem}>Добавить</button>
-    </div>
-  `;
-});
-
-defComponent('todo-list', (ctx) => {
-  const { html, store } = ctx;
-  const todoStore = store('todo');
-
-  return () => html`
-    <ul>
-      ${todoStore.items.value.map(
-    (todo) => html`<li>${todo.title} ${todo.completed ? '✓' : ''}</li>`
-  )}
-    </ul>
-  `;
-});
+    <div>
+      ${todoStore.items.value.map(buildItem)}
+    </div>`
+})
