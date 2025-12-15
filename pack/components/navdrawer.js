dc3bf3679f8babdf98792edd2069028857e47754
Sergey K <kadantsv.sergey@gmail.com>
2025-12-15 15:51:22 +0300

    fix

--- pack/components/navdrawer.js
+++ pack/components/navdrawer.js
@@ -1,32 +1,38 @@
-defStore('navDrawer', ({ signal }) => {
+defStore('useNavdrawer', ({ signal }) => {
     const isOpen = signal(false);
     const toggle = () => isOpen.value = !isOpen.value;
+    const open = () => isOpen.value = true;
+    const close = () => isOpen.value = false;
     return {
         isOpen,
         toggle,
+        open,
+        close,
     };
 });
 
-defComponent('nav-drawer', ({ html, store, slot }) => {
-    const drawerStore = store('navDrawer');
+defComponent('nav-drawer', ({ html, store, slot, $this }) => {
+    const drawer = store('useNavdrawer');
+    $this.drawer = drawer;
 
     return () => html`
-        <div  class=${drawerStore.isOpen.value ? 'drawer is-open' : 'drawer'}>
+        <div  class=${drawer.isOpen.value ? 'drawer is-open' : 'drawer'}>
            ${slot.default}
         </div>
+        <div class=${drawer.isOpen.value ? 'drawer__overlay --opened' : 'drawer__overlay'} onclick=${drawer.close}></div>
     `;
 });
 
 defComponent('toggle-drawer', ({ html, store, effect, $this }) => {
-    const drawerStore = store('navDrawer');
+    const drawer = store('useNavdrawer');
 
     effect(() => {
-        $this.addEventListener('click', drawerStore.toggle);
-        return () => $this.removeEventListener('click', drawerStore.toggle);
+        $this.addEventListener('click', drawer.toggle);
+        return () => $this.removeEventListener('click', drawer.toggle);
     });
 
     return () => html`
-        <button class="${drawerStore.isOpen.value ? 'burger is-open' : 'burger'}" type="button">
+        <button class="${drawer.isOpen.value ? 'burger is-open' : 'burger'}" type="button">
         <span></span>
     </button>
     `;
